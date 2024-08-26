d2dc591480e19255a9281a14469bbbf6458a9281
boguen <qhrms012@naver.com>
2024-08-26 20:52:08 +0900

    [add] 제네릭싱글톤 , 게임매니저

---
+++ Assets/01.Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    private DateTime _sessionStartTime;
+    private DateTime _sessionEndTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sessionStartTime = DateTime.Now;
+        Debug.Log("게임 시작 @: "+DateTime.Now);
+    }
+
+    private void OnApplicationQuit()
+    {
+        _sessionEndTime = DateTime.Now;
+        TimeSpan timeDifference = _sessionEndTime.Subtract(_sessionStartTime);
+    }
+
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Next Scene"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
